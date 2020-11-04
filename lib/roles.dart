a0f4ba4425e0106717c33cdac94742cc8464b4f5
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-03 21:56:38 -0500

    Describes the roles more clearly

--- lib/roles.dart
+++ lib/roles.dart
@@ -1,10 +1,13 @@
 /// The database system has three types of users: [user], [securityOfficer],
 /// [administrator].
-///
-/// [Role.user]: Users who are allowed to perform only DML operations.
-///
-/// [Role.securityOfficer]: Manges users and passwords. Also, they perform
-/// backup and recovery operations.
-///
-/// [Role.administrator]: Perform the tasks of all the above.
-enum Role { user, securityOfficer, administrator }
+enum Role {
+  /// [Role.user]: Users who are allowed to perform only DML operations.
+  user,
+
+  /// [Role.securityOfficer]: Manges users and passwords. Also, they perform
+  /// backup and recovery operations.
+  securityOfficer,
+
+  /// [Role.administrator]: Perform the tasks of all the above.
+  administrator,
+}
