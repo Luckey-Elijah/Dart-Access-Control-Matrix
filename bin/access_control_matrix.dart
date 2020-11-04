5ae84c8c4aba9ed0a5a329247d27177ce44cec14
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-04 11:29:04 -0500

    Running tests on classes

--- bin/access_control_matrix.dart
+++ bin/access_control_matrix.dart
@@ -1,1 +1,12 @@
-void main(List<String> arguments) {}
+import 'package:access_control_matrix/access_control_matrix.dart';
+
+void main(List<String> arguments) {
+  var acm = AccessControlMatrix();
+
+  acm.addSubjects([
+    AcmSubject(Role.administrator, 'user1', ['data'], [], [], []),
+    AcmSubject(Role.administrator, 'user2', ['data'], [], [], []),
+  ]);
+
+  acm.printUsers();
+}
