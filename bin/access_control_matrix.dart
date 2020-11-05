2c083b7a4f1e67e20e5cc0a17966bd21c2ad56aa
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-05 10:56:28 -0500

    adds ojects + subjects for testing

--- bin/access_control_matrix.dart
+++ bin/access_control_matrix.dart
@@ -3,10 +3,21 @@ import 'package:access_control_matrix/access_control_matrix.dart';
 void main(List<String> arguments) {
   var acm = AccessControlMatrix();
 
-  acm.addSubjects([
-    AcmSubject(Role.administrator, 'user1', ['data'], [], [], []),
-    AcmSubject(Role.administrator, 'user2', ['data'], [], [], []),
+  // Columns
+  acm.addObjects([
+    AcmObject('object-1', ['some', 'data'], [], [], []),
+    AcmObject('Obj-2', ['some', 'data'], [], [], []),
+    AcmObject('object-3', ['some', 'data'], [], [], []),
   ]);
 
-  acm.printUsers();
+  // Rows
+  acm.addSubjects([
+    AcmSubject(Role.user, 'user-1', ['data'], [], [], []),
+    AcmSubject(Role.user, 'user-2', ['data'], [], [], []),
+    AcmSubject(Role.securityOfficer, 'officer-1', ['data'], [], [], []),
+    AcmSubject(Role.securityOfficer, 'Officer-02', ['data'], [], [], []),
+    AcmSubject(Role.administrator, 'admin', ['data'], [], [], []),
+  ]);
+
+  acm.printAcm();
 }
