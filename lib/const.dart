ce5ab6c226d3207ca6667416bfb20c62019b0351
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-05 10:56:57 -0500

    Adds runes for printing the table

---
+++ lib/const.dart
@@ -0,0 +1,19 @@
+part of 'access_control_matrix.dart';
+
+/// Unicode characters used to pretty printing.
+class Runes {
+  static const horizontalLine = '─';
+  static const verticalLine = '│';
+
+  static const cornerTopLeft = '┌';
+  static const cornerBotLeft = '└';
+  static const cornerTopRight = '┐';
+  static const cornerBotRight = '┘';
+
+  static const cross = '┼';
+
+  static const wallTop = '┬';
+  static const wallRight = '┤';
+  static const wallLeft = '├';
+  static const wallBot = '┴';
+}
