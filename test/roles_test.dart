94ff4efddd725069da9933c0279efedf6d8b994a
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-08 20:35:15 -0500

    Adds test on role enum

--- test/roles_test.dart
+++ test/roles_test.dart
@@ -1,1 +1,34 @@
+import 'package:access_control_matrix/access_control_matrix.dart';
+import 'package:test/test.dart';
 
+void main() {
+  group('Roles print string should be human-readable.', () {
+    test('User role string', () {
+      final user = Role.user;
+      expect(user.string(), 'USER');
+    });
+    test('Securtiy officer role string', () {
+      final officer = Role.securityOfficer;
+      expect(officer.string(), 'SECURITY OFFICER');
+    });
+    test('Administrator role string', () {
+      final admin = Role.administrator;
+      expect(admin.string(), 'ADMINISTRATOR');
+    });
+  });
+
+  group('Roles print acronym should be one letter', () {
+    test('User role acronym', () {
+      final user = Role.user;
+      expect(user.acronym(), 'U');
+    });
+    test('Security officer role acronym', () {
+      final officer = Role.securityOfficer;
+      expect(officer.acronym(), 'O');
+    });
+    test('Administrator role acronym', () {
+      final admin = Role.administrator;
+      expect(admin.acronym(), 'A');
+    });
+  });
+}
